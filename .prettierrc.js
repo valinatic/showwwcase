25678a68bc3fd94d46f1aada53d01327f28b04c2
yuxxeun <itsyuxxeun@gmail.com>
2022-12-09 12:54:05 +0700

    feat: husky

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+const config = {
+	semi: false,
+	tabWidth: 2,
+	printWidth: 120,
+	singleQuote: true,
+	endOfLine: auto,
+	arrowParens: 'always',
+	trailingComma: 'none'
+}
+
+module.exports = config
